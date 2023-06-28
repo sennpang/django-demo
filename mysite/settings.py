f4afeebc9bb9184386fbe195a7e7c503a7879807
yongzs <673262625@qq.com>
2023-06-28 10:35:49 +0800

    add app polls

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        "NAME": "django",
+        "USER": "root",
+        "PASSWORD": "ai673262625",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
